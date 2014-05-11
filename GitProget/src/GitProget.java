17a3a6bd74a1b1e93f9c76f05bfa83e99d1029ca
Jose Luis <joseluescu@gmail.com>
2014-05-11 13:14:26 +0200

    explicamos

--- GitProget/src/GitProget.java
+++ GitProget/src/GitProget.java
@@ -4,6 +4,8 @@ public class GitProget {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola soy Jose Luis");
+
+System.out.println("seguimos a lo nuestro");
 	}
 
 }
