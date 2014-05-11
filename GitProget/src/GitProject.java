b675749a4d5fb5e15b7cc597c771bbebc61f65fd
Jose Luis <joseluescu@gmail.com>
2014-05-11 13:25:42 +0200

    cambio de nombre

--- GitProget/src/GitProject.java
+++ GitProget/src/GitProject.java
@@ -1,5 +1,5 @@
 
-public class GitProget {
+public class GitProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -8,6 +8,8 @@ System.out.println("Hola soy Jose Luis");
 System.out.println("seguimos a lo nuestro");
 
 System.out.println("vamos a las ramas");
+
+System.out.println("He intentado cambiar nombre");
 	}
 
 }
